1692ed4f7a90204434729a23194c3f881b9c3e1f
Lenni <lenni@mogularena.com>
2018-09-11 20:25:24 +0200

    Update SASS & continue TodoList

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <input type="text" class="todo-input" placeholder="What need to be done">
-      ToDo List goes here
+    <input type="text" class="todo-input" placeholder="What need to be done"
+    v-model="newTodo" @keyup.enter="addTodo">
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+      {{ todo.title }}
+    </div>
   </div>
 </template>
 
@@ -11,7 +14,24 @@ export default {
   name: 'todo-list',
   data () {
     return {
-      msg: 'Welcome to the ToDo App'
+      newTodo: '',
+      todos:[
+        {
+          'id': 1,
+          'title': 'Finish Vue Screencase',
+          'completed': false,
+        },
+        {
+          'id':2,
+          'title': 'Take over world',
+          'completed':false,
+        },
+      ]
+    }
+  },
+  methods:{
+    addTodo(){
+      alert('added');
     }
   }
 }
@@ -19,5 +39,17 @@ export default {
 
 
 <style lang="scss">
+
+.todo-input{
+  width:100%;
+  padding:10px 18px;
+  font-size: 18px;
+  margin-bottom:16px;
+
+  &:focus{
+    outline:0;
+  }
+}
+
 
 </style>
